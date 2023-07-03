c0d1c6156d998658b73b0745a9b4ba4cfffa42fe
YeonChanLim <yclim1010@naver.com>
2023-07-03 20:09:16 +0900

    feat: solved 1 BOJ Week02 start

---
+++ YeonChanLim/Week02/BOJ_1018.java
@@ -0,0 +1,65 @@
+package YeonChanLim.Week02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1018 {
+    static int N,M;
+    static boolean[][] arr;
+    static int min = 64;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+
+            for (int j = 0; j < M; j++) {
+                if(str.charAt(j) == 'W'){
+                    arr[i][j] = true;
+                } else{
+                    arr[i][j] = false;
+                }
+            }
+        }
+
+        int N_row = N - 7;
+        int M_col = M - 7;
+
+        for (int i = 0; i < N_row; i++) {
+            for (int j = 0; j < M_col; j++) {
+                find(i,j);
+            }
+        }
+        System.out.println(min);
+    }
+
+    public static void find(int x, int y){
+        int end_x = x + 8;
+        int end_y = y + 8;
+        int count = 0;
+
+        boolean TF = arr[x][y];
+
+        for (int i = x; i < end_x; i++) {
+            for (int j = y; j < end_y; j++) {
+                if(arr[i][j] != TF){
+                    count++;
+                }
+                TF = (!TF);
+            }
+            TF = !TF;
+        }
+        count = Math.min(count, 64 - count);
+
+        min = Math.min(min,count);
+    }
+}
