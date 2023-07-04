abb4f5a4015d525b028a55134d33bbb4159f9490
YeonChanLim <yclim1010@naver.com>
2023-07-04 21:12:55 +0900

    feat: solved 1 BOJ

---
+++ YeonChanLim/Week02/BOJ_1181.java
@@ -0,0 +1,39 @@
+package YeonChanLim.Week02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class BOJ_1181 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+
+        String[] arr = new String[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = br.readLine();
+        }
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                if(s1.length() == s2.length()) {
+                    return s1.compareTo(s2);
+                }else {
+                    return s1.length() - s2.length();
+                }
+
+            }
+        });
+        System.out.println(arr[0]);
+        for(int i = 1; i < N; i++) {
+            if (!arr[i].equals(arr[i - 1])) {
+                System.out.println(arr[i]);
+            }
+        }
+    }
+}
