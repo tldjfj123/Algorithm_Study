7da0bc246220a17010badb49c717a37c460a9228
YeonChanLim <yclim1010@naver.com>
2023-07-08 23:46:54 +0900

    feat: solving 1 BOJ

---
+++ YeonChanLim/Week02/BOJ_1676.java
@@ -0,0 +1,14 @@
+package YeonChanLim.Week02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_1676 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+
+    }
+}
