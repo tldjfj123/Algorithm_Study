5d9411264e8433bc785f893e95d842a059ef5655
Seongjun Ahn <dksckd0804@naver.com>
2023-08-02 18:08:08 +0900

    feat : solved 덧칠하기 with Java

---
+++ tldjfj123/Week05/덧칠하기/Solution.java
@@ -0,0 +1,21 @@
+package 덧칠하기;
+
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int start = section[0];
+        int end = section[0] + (m-1);
+        int ans = 1;
+
+        for(int i : section){
+            if(i>=start && i<=end)
+                continue;
+            else{
+                start = i;
+                end = i + (m-1);
+                ans++;
+            }
+
+        }
+        return ans;
+    }
+}
