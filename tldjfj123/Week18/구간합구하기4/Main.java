cc30523ed1693679c2cc2c65a17af9a03629a926
Seongjun Ahn <dksckd0804@naver.com>
2023-10-21 20:03:50 +0900

    feat : solved 구간 합 구하기 4 with Java

---
+++ tldjfj123/Week18/구간합구하기4/Main.java
@@ -0,0 +1,40 @@
+package 구간합구하기4;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n + 1];
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n+1];
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = arr[i] + dp[i-1];
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            System.out.println(dp[end] - dp[start-1]);
+
+        }
+
+    }
+}
