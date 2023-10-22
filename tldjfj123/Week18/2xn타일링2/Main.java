c6e1c4d7954dbd75fc405c079ad3ce671f97246b
Seongjun Ahn <dksckd0804@naver.com>
2023-10-22 13:03:19 +0900

    feat : solved 2xn타일링2 with Java

--- tldjfj123/Week18/2xn타일링2/Main.java
+++ tldjfj123/Week18/2xn타일링2/Main.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -11,12 +10,12 @@ public class Main {
 
         int[] dp = new int[1001];
         dp[1] = 1;
-        dp[2] = 2;
+        dp[2] = 3;
+
         for (int i = 3; i <= 1000; i++) {
-            dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+            dp[i] = ((dp[i-1] % 10007) + 2 * (dp[i-2] % 10007)) % 10007;
         }
 
         System.out.println(dp[n]);
     }
 }
-
